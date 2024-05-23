c7f0bcfbbad3d75c91f77c718dbda5bde1fa23b1
JustinNQ <1546021@senati,pe>
2024-05-23 16:19:55 -0500

    variables

---
+++ 02-variables.php
@@ -0,0 +1,10 @@
+<?php
+
+$nombres ="Justin Gianpiero.";
+$apellidos="Nina Quiroz";
+$curso="BACKEND DEVELOPER WEB";
+$semestre="TERCER SEMESTRE";
+//juntar nombres apellidos curso y semestre
+echo $nombres." ".$apellidos." ".$curso." ".$semestre;
+
+?>
