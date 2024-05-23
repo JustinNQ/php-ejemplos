da7ee2b64deb8ba4738337230b6704a3c9e8f892
JustinNQ <1546021@senati,pe>
2024-05-23 17:21:35 -0500

    polimorfismo

---
+++ 15-polimorfismo.php
@@ -0,0 +1,42 @@
+<?php
+
+
+
+include("15-polimorfismo.php");
+
+echo "AREA DE TRIANGULO\n";
+echo "INGRESE LA BASE :";
+$b=fgets(STDIN);
+echo "INGRESE LA ALTURA :";
+$h=fgets(STDIN);
+$triangulo = new Triangulo($b,$h);
+$area=$triangulo->calcularArea();
+echo "EL AREA DEL TRIANGULO ES ".$area;
+
+echo "AREA DE RECTANGULO\n";
+echo "INGRESE LA BASE :";
+$b=fgets(STDIN);
+echo "INGRESE LA ALTURA :";
+$a=fgets(STDIN);
+$rectangulo = new Rectangulo($b,$a);
+$area=$rectangulo->calcularArea();
+echo "EL AREA DEL RECTANGULO ES ".$area;
+
+
+echo "AREA DE CUADRADO\n";
+echo "INGRESE EL LADO :";
+$l = trim(fgets(STDIN));
+$cuadrado = new Cuadrado($l);
+$areaCuadrado = $cuadrado->calcularArea();
+echo "EL AREA DEL CUADRADO ES " . $areaCuadrado . "\n";
+
+
+echo "AREA DE CIRCULO\n";
+echo "INGRESE EL RADIO :";
+$r = trim(fgets(STDIN));
+$circulo = new Circulo($r);
+$areaCirculo = $circulo->calcularArea();
+echo "EL AREA DEL CIRCULO ES " . $areaCirculo . "\n";
+
+
+?>
